efe037da01ae7bfff4e4b2541057e175226ec374
thomazpicelli <thomazpicelli@gmail.com>
2015-07-23 00:19:02 -0300

    _layout 1

---
+++ Cinema/Cinema.UI/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.UI.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
